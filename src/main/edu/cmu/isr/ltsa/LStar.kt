baccfa898b22c71f87116c6b2997818ac16224f6
Changjian Zhang <mljzcj@gmail.com>
2019-08-31 11:17:52 -0400

    Reproduce the same results!

--- src/main/edu/cmu/isr/ltsa/LStar.kt
+++ src/main/edu/cmu/isr/ltsa/LStar.kt
@@ -21,21 +21,19 @@ class LStar(val M1: String, val M2: String, val P: String) {
       println("========== Find assumption for M1 ==========")
       println("Σ of the language: ${Σ}")
       val A = lstar()
+      val A_fsp = conjectureToFSP(A)
       println("========== Validate assumption with the environment M2 ==========")
-      val counterExample = ltsaCall.propertyCheck(ltsaCall.doCompile("$M2\nproperty $A"))
+      val counterExample = ltsaCall.propertyCheck(ltsaCall.doCompile("$M2\nproperty $A_fsp"))
       if (counterExample == null) {
-        println("========== Find the weakest assumption for M1 ==========\n$A")
-        return A
+        println("========== Find the weakest assumption for M1 ==========\n$A_fsp")
+        return A_fsp
       } else {
         println("========== Counterexample found with environment M2 ==========\n$counterExample")
         val projected = counterExample.filter { it in Σ }
         val A_c = "AC = (${projected.joinToString(" -> ")} -> AC) + {${Σ.joinToString(", ")}}."
         if (ltsaCall.propertyCheck(ltsaCall.doCompile("$A_c\n$M1\n$P")) == null) {
           println("========== Weaken the assumption for M1 ==========")
-//          T[projected.joinToString(",")] = false
-//          witnessOfCounterExample(projected)
-          S.add(projected.joinToString(","))
-          T[projected.joinToString(",")] = true
+          witnessOfCounterExample(A, projected)
         } else {
           throw Error("P is violated in M1 || M2.")
         }
@@ -43,7 +41,7 @@ class LStar(val M1: String, val M2: String, val P: String) {
     }
   }
 
-  fun lstar(): String {
+  fun lstar(): Set<Triple<String, String, String>> {
     while (true) {
       update_T_withQueries()
       while (true) {
@@ -56,78 +54,105 @@ class LStar(val M1: String, val M2: String, val P: String) {
       val C = buildConjecture()
       val counterExample = checkCorrectness(C)
       if (counterExample == null) {
-        println("========== Target language U found ==========\n$C")
         return C
       } else {
         println("Counterexample found: $counterExample")
-        witnessOfCounterExample(counterExample)
+        witnessOfCounterExample(C, counterExample)
       }
     }
   }
 
-  private fun witnessOfCounterExample(counterExample: List<String>) {
+  private fun witnessOfCounterExample(C: Set<Triple<String, String, String>>, counterExample: List<String>) {
     val projected = counterExample.filter { it in Σ }
+    val size = projected.size
     println("Projected counterexample: $projected")
-    for (i in 1 until projected.size) {
-      val qi = projected.subList(0, i).joinToString(",")
-      val qi1 = projected.subList(0, i + 1).joinToString(",")
-      if (membershipQuery(qi) && !membershipQuery(qi1)) {
-        S.add(qi)
-        E.add(projected.subList(i, projected.size).joinToString(","))
+    for (i in projected.indices) {
+      val si_1 = getStateAfterTrace(C, projected.subList(0, i))
+      val si = getStateAfterTrace(C, projected.subList(0, i + 1))
+      val qi_1 = membershipQuery(concat(si_1, *projected.subList(i, size).toTypedArray()))
+      val qi = membershipQuery(concat(si, *projected.subList(i + 1, size).toTypedArray()))
+      if (qi_1 != qi) {
+        S.add(concat(si_1, projected[i]))
+        E.add(concat(*projected.subList(i + 1, size).toTypedArray()))
         println("By witness counter example, S = $S, E = $E")
         return
       }
     }
   }
 
-  private fun checkCorrectness(C: String): List<String>? {
-    val ltsaCall = LTSACall()
-    return ltsaCall.propertyCheck(ltsaCall.doCompile("$C\n$M1\n$P"))
+  private fun getStateAfterTrace(C: Set<Triple<String, String, String>>, trace: List<String>): String {
+    var s = ""
+    for (a in trace) {
+      s = C.find { it.first == s && it.second == a }!!.third
+    }
+    return s
   }
 
-  private fun buildConjecture(): String {
+  private fun checkCorrectness(C: Set<Triple<String, String, String>>): List<String>? {
+    val ltsaCall = LTSACall()
+    val fsp = conjectureToFSP(C)
+    return ltsaCall.propertyCheck(ltsaCall.doCompile("$fsp\n$M1\n$P"))
+  }
+
+  private fun buildConjecture(): Set<Triple<String, String, String>> {
+    // Omit the error states
     val F = S.filter { T[it] == true }
-    val F_map = F.foldIndexed(mutableMapOf<String, String>()) { i, m, s ->
-      m[s] = if (s == "") "C" else "C$i"
-      m
-    }
     val δ = mutableSetOf<Triple<String, String, String>>()
+    // Generate transitions of the conjecture automaton
+    // The transition relation δ is defined as δ(s, a) = s' where \forall e \in E: T(sae) = T(s'e).
     for (s in F) {
       for (a in Σ) {
         val s_ = S.find { s_ -> E.forall { e -> T[concat(s, a, e)] == T[concat(s_, e)] } }
         if (s_ in F) {
-          δ.add(Triple(F_map[s]!!, a, F_map[s_]!!))
+          δ.add(Triple(s, a, s_!!))
         }
       }
     }
     println("Desire state machine: $δ")
+    return δ
+  }
+
+  private fun conjectureToFSP(δ: Set<Triple<String, String, String>>): String {
+    // Final states of the conjecture automaton
+    val F = δ.map { it.first }.toSet()
+    // Since F \subseteq S where S \subseteq Σ*, thus map F to other names to generate the FSP spec.
+    val F_map = F.foldIndexed(mutableMapOf<String, String>()) { i, m, s ->
+      m[s] = if (s == "") "C" else "C$i"
+      m
+    }
+    // Divide the FSP spec into several sub processes, like A = (a -> B), B = (b -> A).
     val sm = F_map.values.fold(mutableMapOf<String, MutableList<String>>()) { m, s ->
       m[s] = mutableListOf()
       m
     }
     for (t in δ) {
-      sm[t.first]!!.add("${t.second} -> ${t.third}")
+      sm[F_map[t.first]]!!.add("${t.second} -> ${F_map[t.third]}")
     }
+    // Build the FSP spec, C is the main process which must exist.
     val C = StringBuilder("C = (")
     C.append(sm["C"]!!.joinToString(" | "))
     C.append(')')
-    val C_tmp = sm.filter { it.key != "C" && it.value.isNotEmpty() }
+    // Other sub-process
+    val C_tmp = sm.filter { it.key != "C" }
     if (C_tmp.isNotEmpty()) {
       C.append(",\n")
       val subs = C_tmp.map { "${it.key} = (${it.value.joinToString(" | ")})" }
       C.append(subs.joinToString(",\n"))
     }
+    // Add the alphabet
     C.append(" + {${Σ.joinToString(", ")}}")
     C.append('.')
-    println("Constructed state machine: $C")
+    println("Constructed state machine:\n$C")
     return C.toString()
   }
 
   private fun isClosed(): Set<String> {
     val sa = mutableSetOf<String>()
+    // (S, E, T) is closed when \forall s \in S, \forall a \in Σ, \exists s' \in S, \forall e \in E: T(sae) = T(s'e)
     S.forall { s ->
       Σ.forall { a ->
         val re = S.exists { s_ -> E.forall { e -> T[concat(s, a, e)] == T[concat(s_, e)] } }
+        // if (S, E, T) is not closed, add sa to S.
         if (!re) {
           sa.add(concat(s, a))
         }
@@ -143,6 +168,7 @@ class LStar(val M1: String, val M2: String, val P: String) {
   }
 
   private fun update_T_withQueries() {
+    // Update T by making membership queries on (S \cup S . Σ) . E
     val queries = (S union S.flatMap { s -> Σ.map { a -> concat(s, a) } })
       .flatMap { s -> E.map { e -> concat(s, e) } }
     for (query in queries) {
