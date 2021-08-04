0241ecdc894c569f3eda8bfed1c083375171e900
tarangsaluja <86010842+tarangsaluja@users.noreply.github.com>
2021-08-04 16:02:56 -0400

    therac 25 update (#8)

--- robustness-repair/models/therac25/run.py
+++ robustness-repair/models/therac25/run.py
@@ -3,17 +3,39 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 
-from repair import Repair
+from repair import *
 
 alphabet = set([
     "hPressX", "hPressE", "hPressEnter", "hPressB", "mFire", "hPressUp", "hPressUp1", "mEBeamLvl", "mXrayLvl",
     "mInPlace", "mOutPlace", "mInitXray", "mInitEBeam"
 ])
 r = Repair(
-    plant=["interface.lts", "power.lts", "env.lts"],
-    property=["p.lts"],
+    sys= ["interface.lts", "power.lts"],
+    env_p = ["env.lts"], 
+    safety =["p.lts"],
+    preferred={   # rank the preferred behavior by importance
+        PRIORITY3: ["back.lts", "fire.lts"],
+        PRIORITY2: ["back1.lts"],
+        PRIORITY1: [],  
+        PRIORITY0: []
+    },
+    progress=[],
     alphabet=alphabet,
-    controllable=set(["hPressX", "hPressE", "hPressEnter", "hPressB", "hPressUp", "hPressUp1"]),
-    observable=alphabet
+    controllable={  # rank the controllable events by cost
+        PRIORITY3: ["hPressX", "hPressE", "hPressEnter", "hPressB"],
+        PRIORITY2: ["hPressUp", "hPressUp1"],
+        PRIORITY1: ["mFire", "mEBeamLvl", "mXrayLvl","mInPlace", "mOutPlace", "mInitXray","mInitEBeam"],
+        PRIORITY0: []
+    },
+    observable={    # rank observable events by cost
+        PRIORITY3: [],
+        PRIORITY2:  ["hPressX", "hPressE", "hPressEnter", "hPressB"],
+        PRIORITY1: ["hPressUp", "hPressUp1"],
+        PRIORITY0: ["mFire", "mEBeamLvl", "mXrayLvl","mInPlace", "mOutPlace", "mInitXray","mInitEBeam"] 
+    }
 )
-r.synthesize()
+
+
+result = r.synthesize(6)
+for c in result:
+    print(r.fsm2fsp(c["M_prime"], c["observable"], name="M"))
